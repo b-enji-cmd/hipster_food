0f106fad313966e72840afcaf9c0d9e1fc81154d
Ben Fulton <benjamin.fulton99@gmail.com>
2021-01-12 13:19:31 -0700

    Add stock testing

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -12,4 +12,38 @@ class FoodTruckTest < Minitest::Test
 	def test_it_is
 		assert_instance_of FoodTruck, @food_truck
 	end
+
+	def test_it_has_things
+		assert_equal "Rocky Mountain Pies", @food_truck.name
+		expected = {}
+		assert_equal expected, @food_truck.inventory
+	end
+
+	def test_it_can_check_stock
+		assert_equal 0, @food_truck.check_stock(@item1)
+	end
+
+	def test_it_can_stock
+		@food_truck.stock(@item1, 30)
+		expected = {
+			@item1 => 30
+		}
+
+		assert_equal expected, @food_truck.inventory
+		assert_equal 30, @food_truck.check_stock(@item1)
+	end
+
+	def test_it_can_increment_stock
+		@food_truck.stock(@item1, 30)
+		@food_truck.stock(@item1, 25)
+		assert_equal 55, @food_truck.check_stock(@item1)
+		@food_truck.stock(@item2, 12)
+
+		expected = {
+			@item1 => 55,
+			@item2 => 12
+		}
+
+		assert_equal expected, @food_truck.inventory
+	end
 end
