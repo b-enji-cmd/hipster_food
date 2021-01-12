c30a951648716f6cfca5ddb5e80d88be013a5d22
Ben Fulton <benjamin.fulton99@gmail.com>
2021-01-12 13:32:40 -0700

    Add food truck collection method for event

---
+++ lib/event.rb
@@ -0,0 +1,13 @@
+class Event
+	attr_reader :name,
+				:food_trucks
+	def initialize(name)
+		@name = name
+		@food_trucks = []
+	end
+
+	def add_food_truck(truck)
+		@food_trucks << truck
+	end
+	
+end
