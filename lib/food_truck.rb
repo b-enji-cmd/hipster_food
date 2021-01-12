9347a60225c9cbbf00df34a0c3ee75f8d63efb4c
Ben Fulton <benjamin.fulton99@gmail.com>
2021-01-12 13:23:18 -0700

    Add potential revenue method

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,6 +1,7 @@
 class FoodTruck
 	attr_reader :name,
 				:inventory
+
 	def initialize(name)
 		@name = name
 		@inventory = Hash.new{0}
@@ -12,5 +13,11 @@ class FoodTruck
 
 	def stock(item, quantity)
 		@inventory[item] += quantity
+	end
+
+	def potential_revenue
+		@inventory.sum do |key,value|
+			key.price * value
+		end
 	end
 end
