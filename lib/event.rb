577a960ad889926afe00ed7f098bc595dde4e1b2
Ben Fulton <benjamin.fulton99@gmail.com>
2021-01-12 14:53:26 -0700

    Add total_inventory method

--- lib/event.rb
+++ lib/event.rb
@@ -21,5 +21,46 @@ class Event
 			truck.check_stock(item) > 0
 		end
 	end
-	
+
+	def get_item_names
+		@food_trucks.flat_map do |truck|
+			truck.inventory.map do |item,quantity|
+				item.name
+			end
+		end.uniq
+	end
+
+	def sorted_item_list
+		items = get_item_names
+		items.sort { |a, b| a <=> b } #this is preforming an in place swap using -1, 0 or 1 
+																	# depending on the relation of the two block variables
+	end
+
+	def inventory_items
+		@food_trucks.flat_map do |truck|
+			truck.inventory.keys
+		end.uniq
+	end
+
+	def generate_hash
+		inventory_items.collect do |item| 
+			[item , {
+				quantity: 0,
+				food_trucks: []
+				}]
+			end	
+	end
+
+	def total_inventory
+		breakdown = Hash[generate_hash]
+			
+		@food_trucks.each do |truck|
+       truck.inventory.each do |item, quantity|
+         breakdown[item][:quantity] += quantity
+         breakdown[item][:food_trucks] << truck
+       end
+     end
+     	breakdown
+	end
+
 end
